6281aae74983a9c71dad60c6443b462e1ba1b3e2
Kumar Gaurav <kmrgaurav11235@gmail.com>
2021-02-26 14:56:48 +0530

    Logging with slf4j and logback

--- 01-hello-maven/src/main/java/in/gaurav/HelloMaven.java
+++ 01-hello-maven/src/main/java/in/gaurav/HelloMaven.java
@@ -1,7 +1,14 @@
 package in.gaurav;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloMaven {
+
+    private static final Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        log.info("Hello Info.");
+        log.debug("Hello Debug.");
     }
 }
